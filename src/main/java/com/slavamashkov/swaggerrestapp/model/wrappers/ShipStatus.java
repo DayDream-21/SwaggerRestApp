dd17d6a35098427291d1cc42b674c0abb8c2a490
DayDream-21 <vova13158@gmail.com>
2022-07-26 15:24:57 +0300

    Wrappers
    Create and update wrapper classes

---
+++ src/main/java/com/slavamashkov/swaggerrestapp/model/wrappers/ShipStatus.java
@@ -0,0 +1,12 @@
+package com.slavamashkov.swaggerrestapp.model.wrappers;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ShipStatus {
+    String status;
+}
