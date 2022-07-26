185b04e235ccd6daa7e600736ab61f529f3b4b95
DayDream-21 <vova13158@gmail.com>
2022-07-26 15:23:20 +0300

    Test data
    Update test-data.sql in accordance with updated entities

--- documents/test-data.sql
+++ documents/test-data.sql
@@ -1,37 +1,29 @@
-CREATE TABLE navy.ports
-(
-    id       bigserial    NOT NULL,
-    "name"   varchar(255) NOT NULL,
-    capacity int4         NOT NULL,
-    CONSTRAINT port_pk PRIMARY KEY (id)
-);
+INSERT INTO navy.ports (capacity, name)
+VALUES
+    (5,  'Дальний'),
+    (10, 'Солнечный'),
+    (7,  'Восточный');
 
-CREATE TABLE navy.ships
-(
-    id      bigserial    NOT NULL,
-    "name"  varchar(255) NOT NULL,
-    port_id int8         NULL,
-    status  varchar(255) NOT NULL,
-    CONSTRAINT ship_pk PRIMARY KEY (id),
-    CONSTRAINT ship_fk_port FOREIGN KEY (port_id) REFERENCES navy.ports (id)
-);
+INSERT INTO navy.ships (max_crew_capacity, min_crew_capacity, name, status, port_id)
+VALUES
+    (5, 2, 'Проворный',   'SEA',  NULL),
+    (5, 2, 'Санта Мария', 'PORT', 1),
+    (5, 2, 'Туман',       'PORT', 2),
+    (5, 2, 'Буря',        'PORT', 2),
+    (5, 2, 'Сказочный',   'PORT', 2),
+    (5, 2, 'Грозный',     'PORT', 3),
+    (5, 2, 'Восток',      'PORT', 3),
+    (5, 2, 'Кудесник',    'PORT', 3),
+    (5, 2, 'Арктика',     'PORT', 3),
+    (5, 2, 'Москва',      'PORT', 2),
+    (5, 2, 'Беда',        'PORT', 2),
+    (5, 2, 'Красивый',    'PORT', 2);
 
-INSERT INTO navy.ports (name, capacity)
-VALUES ('Дальний', 5),
-       ('Солнечный', 10),
-       ('Восточный', 7);
-
-INSERT INTO navy.ships (name, status, port_id)
-VALUES ('Проворный', 'PORT', 1),
-       ('Санта Мария', 'PORT', 1),
-       ('Туман', 'PORT', 1),
-       ('Буря', 'PORT', 1),
-       ('Сказочный', 'PORT', 1),
-       ('Грозный', 'PORT', 2),
-       ('Восток', 'PORT', 2),
-       ('Кудесник', 'PORT', 3),
-       ('Арктика', 'SEA', NULL),
-       ('Москва', 'PORT', 3);
-
-ALTER TABLE navy.ships
-    ALTER COLUMN status TYPE varchar(255);
+INSERT INTO navy.crew_members (status, name, role, ship_id)
+VALUES
+    ('ON_SHIP', 'Ivan',   'CAPITAN', 1),
+    ('ON_LAND', 'Anton',  'CAPITAN', null),
+    ('ON_SHIP', 'Slava',  'MATE',    1),
+    ('ON_SHIP', 'Dima',   'MATE',    1),
+    ('ON_SHIP', 'Maksim', 'MATE',    2),
+    ('ON_SHIP', 'Jenya',  'MATE',    2);
